32630c7481aeb07f1b82b856b1eecf726edc195f
Ashley <a.teraishi@gmail.com>
2022-06-06 19:31:21 -0700

    local run

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const SERVER_URL = 'https://cst438-gradebook-be.herokuapp.com/';
-//export const SERVER_URL = 'http://localhost:8081/';
+//export const SERVER_URL = 'https://cst438-gradebook-be.herokuapp.com/';
+export const SERVER_URL = 'http://localhost:8081/';
